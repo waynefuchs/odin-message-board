d6a443fcb7254a4adbc4e978940590aa64a01e9c
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-12-21 01:21:55 -0500

    Add bcrypt and jsonwebtoken, and add api route

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
+// `password` is the name of the POST res.body variable (but is never stored in the database!)
 const UserSchema = new mongoose.Schema({
   email: { type: String },
+  verified: { type: Boolean },
   name: { type: String },
-  password_hash: { type: String },
+  password: { type: String },
+  hash: { type: String },
+  session: { type: String },
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,6 @@
 import dbConnect from "../../../lib/dbConnect";
 import User from "../../../models/User";
+import bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
   await dbConnect();
@@ -12,14 +13,35 @@ export default async function handler(req, res) {
         res.status(400).json({ success: false });
       }
       break;
+
     case "POST":
+      const { email, name, password } = req.body;
+      if (!email) throw new Error("Email is required");
+      if (!name) throw new Error("Name is required");
+      if (!password) throw new Error("Password is required");
+      if (!process.env.SALT_ROUNDS) throw new Error("SALT_ROUNDS not found");
+
+      const verified = false;
+      const session = false;
+      const saltRounds = Number(process.env.SALT_ROUNDS);
+
+      // Get the hash
       try {
-        const user = await User.create(req.body);
+        const hash = await bcrypt.hash(password, saltRounds);
+        const user = await User.create({
+          email,
+          verified,
+          name,
+          password,
+          hash,
+          session,
+        });
         res.status(201).json({ success: true, data: user });
       } catch (error) {
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, error });
       }
       break;
+
     default:
       res.status(400).json({ success: false });
       break;
