7e4ad83da4c90e74918cd01e1a46232890e4bb14
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-27 22:52:52 -0500

    Initial Commit

--- models/Post.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const PostSchema = new mongoose.Schema({
-  user: { type: String },
-  message: { type: String },
-  posted: { type: Date },
-});
-
-export default mongoose.models.Post || mongoose.model("Post", PostSchema);
